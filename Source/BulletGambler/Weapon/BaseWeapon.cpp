d455526c99e9627d75c1c0054b986534015980cc
luiscastellar <71766787+luiscastellar@users.noreply.github.com>
2025-10-13 16:31:04 +0200

    feat (weapon): pickable weapon in creation

--- Source/BulletGambler/Weapon/BaseWeapon.cpp
+++ Source/BulletGambler/Weapon/BaseWeapon.cpp
@@ -2,6 +2,8 @@
 
 #include "BaseWeapon.h"
 #include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+#include "BulletGambler/Characters/BaseCharacter.h"
 
 ABaseWeapon::ABaseWeapon()
 {
@@ -21,20 +23,38 @@ ABaseWeapon::ABaseWeapon()
 	AreaSphere->SetupAttachment(RootComponent);
 	AreaSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
 	AreaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	PickupWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("PickupWidget"));
+	PickupWidget->SetupAttachment(RootComponent);
 }
 
 void ABaseWeapon::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	if (HasAuthority())
+	//if (HasAuthority())
+	//{
+	//	AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	//	AreaSphere->SetCollisionResponseToChannel(ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	//	AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &ABaseWeapon::OnSphereOverlap);
+	//} 
+
+	if (PickupWidget)
 	{
-		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
-		AreaSphere->SetCollisionResponseToChannel(ECC_Pawn, ECollisionResponse::ECR_Overlap);
-	} 
+		PickupWidget->SetVisibility(false);
+	}
 }
 
-// Called every frame
+void ABaseWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ABaseCharacter* BaseCharacter = Cast<ABaseCharacter>(OtherActor);
+	if (BaseCharacter && PickupWidget)
+	{
+		PickupWidget->SetVisibility(true);
+	}
+
+}
+
 void ABaseWeapon::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
